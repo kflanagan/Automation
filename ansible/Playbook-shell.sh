e6f49a3b726f98f5f026f2ca9db6b3d6d744ffdf
Kevin Flanagan <flanagannc@gmail.com>
2024-01-14 07:02:16 -0500

    Wrapper uses a vault now

--- ansible/Playbook-shell.sh
+++ ansible/Playbook-shell.sh
@@ -2,6 +2,10 @@
 # A wrapper for a playbook, so I don't have to remember the syntax
 
 
-ansible-playbook $1  --extra-vars 'ansible_become_pass=$2'
+#ansible-playbook $1  --extra-vars 'ansible_become_pass='$2''
 
+# The passwd.yaml vault is created in the current directory
+# ansible-vault create passwd.yaml
+# See /etc/ansible/hosts for the variables syntax and see the line below to call the vault
+ansible-playbook   --ask-vault-pass --extra-vars '@passwd.yaml' $1 
 
