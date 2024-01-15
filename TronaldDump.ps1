10fb34968c7577462b046d2d3362065bd9ee2cd9
Kevin Flanagan <flanagannc@gmail.com>
2024-01-15 12:06:11 -0500

    Moved Powershell script

--- TronaldDump.ps1
+++
@@ -1,7 +0,0 @@
-﻿# REST Client tests on https://github.com/tronalddump-io/tronald-app
-$baseUrl=https://api.tronalddump.io
-
-### Get a random clever quote
-$response=Invoke-WebRequest -Method Get -Uri $baseurl/random/quote #https://api.tronalddump.io
-write-host $response
-# Accept: application/hal+json
